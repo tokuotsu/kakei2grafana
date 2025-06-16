97a250c9a93dca2b420bada432ceef3db470e757
tokuotsu <t.tokuhiro0317@gmail.com>
2025-06-16 23:31:38 +0900

    バグ修正

--- src/transfer.py
+++ src/transfer.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 if __name__ == "__main__":
     from insert_csv2db import add_credit_withdrawal_info
@@ -59,37 +60,50 @@ def transform_transfer_data(transfer_df):
 
 
 def process_transfer_csv(
-    transfer_csv_file, card_settings_file, output_file, add_credit_info_func=add_credit_withdrawal_info
+    transfer_csv_file_path, 
+    card_settings_file_path, 
+    output_file_path, 
+    add_credit_info_func=add_credit_withdrawal_info
 ):
     """
     振替CSVデータを収支データに変換し、クレジットカードの情報を追加して保存
     
     Parameters:
-        transfer_csv_file (str): 振替データのCSVファイルパス
-        card_settings_file (str): meta.jsonのカード設定ファイルパス
-        output_file (str): 変換後のデータを保存するファイルパス
+        transfer_csv_file_path (str): 振替データのCSVファイルパス
+        card_settings_file_path (str): meta.jsonのカード設定ファイルパス
+        output_file_path (str): 変換後のデータを保存するファイルパス
         add_credit_info_func (function): クレジットカード情報を追加する処理関数
         
     Returns:
         None
     """
     # CSV読み込み
-    transfer_df = pd.read_csv(transfer_csv_file, encoding='utf-8', parse_dates=['日付'])
+    transfer_df = pd.read_csv(transfer_csv_file_path, encoding='utf-8', parse_dates=['日付'])
     
     # 振替データを収支データに変換
     transformed_df = transform_transfer_data(transfer_df)
+    with open(card_settings_file_path, "r") as f:
+        card_settings = json.load(f)["card_settings"]
     
     # クレジットカード関連情報の付加
-    df_with_credit_info = add_credit_info_func(transformed_df, card_settings_file)
+    df_with_credit_info = add_credit_info_func(transformed_df, card_settings)
     
     # CSVに保存
-    df_with_credit_info.to_csv(output_file, index=False)
+    df_with_credit_info.to_csv(output_file_path, index=False)
     print("✅ 振替データの変換と保存が完了しました。")
 
 if __name__ == "__main__":
     from insert_csv2db import add_credit_withdrawal_info
-    transfer_csv_file = '../data/csvoutputs/transfer.csv'
+    import json
+    transfer_csv_file_path = '../data/csvoutputs/transfer.csv'
     card_settings_file = '../meta.json'
-    output_file = '../output/transfer.csv'
-    
-    process_transfer_csv(transfer_csv_file, card_settings_file, output_file)
+    output_file_path = '../output/transfer.csv'
+    with open(card_settings_file, "r") as f:
+        card_settings = json.load(f)["card_settings"]
+    print("iD/Amazon" in card_settings)
+    # exit()
+    process_transfer_csv(
+        transfer_csv_file_path, 
+        card_settings_file, 
+        output_file_path,
+        add_credit_info_func=add_credit_withdrawal_info)
